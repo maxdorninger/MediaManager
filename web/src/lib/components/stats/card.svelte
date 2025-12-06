d1eee14b792003527246afe452df00ee916c9b65
maxid <contact@dorninger.co>
2025-12-06 22:38:28 +0100

    add card to inform user of new versions

--- web/src/lib/components/stats/card.svelte
+++ web/src/lib/components/stats/card.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-    import * as Card from "$lib/components/ui/card/index.js";
+	import * as Card from '$lib/components/ui/card/index.js';
 
-    let {title, content, footer}: { title: string, content: any, footer: string } = $props()
-
+	// eslint-disable-next-line
+	let { title, footer, children }: { title: string; footer: string; children: any } = $props();
 </script>
 
 <Card.Root>
-    <Card.Header>
-        <Card.Title><p>{title}</p></Card.Title>
-    </Card.Header>
-    <Card.Content>
-        <p class="text-2xl font-extrabold leading-0">{content ?? "Error"}</p>
-    </Card.Content>
-    <Card.Footer>
-        <Card.Description >
-            <p>{footer}</p>
-        </Card.Description>
-    </Card.Footer>
+	<Card.Header>
+		<Card.Title><p>{title}</p></Card.Title>
+	</Card.Header>
+	<Card.Content>
+		<p class="text-3xl font-extrabold">{@render children?.()}</p>
+	</Card.Content>
+	<Card.Footer>
+		<Card.Description>
+			<p>{footer}</p>
+		</Card.Description>
+	</Card.Footer>
 </Card.Root>
