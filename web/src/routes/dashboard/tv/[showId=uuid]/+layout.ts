4b7d5eea5478f3e5da1187c30e58b8b2c09d8731
maxid <97409287+maxdorninger@users.noreply.github.com>
2025-12-21 16:16:12 +0100

    refactor: improve async data fetching in load functions

--- web/src/routes/dashboard/tv/[showId=uuid]/+layout.ts
+++ web/src/routes/dashboard/tv/[showId=uuid]/+layout.ts
@@ -2,17 +2,17 @@ import type { LayoutLoad } from './$types';
 import client from '$lib/api';
 
 export const load: LayoutLoad = async ({ params, fetch }) => {
-	const show = await client.GET('/api/v1/tv/shows/{show_id}', {
+	const show = client.GET('/api/v1/tv/shows/{show_id}', {
 		fetch: fetch,
 		params: { path: { show_id: params.showId } }
 	});
-	const torrents = await client.GET('/api/v1/tv/shows/{show_id}/torrents', {
+	const torrents = client.GET('/api/v1/tv/shows/{show_id}/torrents', {
 		fetch: fetch,
 		params: { path: { show_id: params.showId } }
 	});
 
 	return {
-		showData: show.data,
-		torrentsData: torrents.data
+		showData: await show.then((x) => x.data),
+		torrentsData: await torrents.then((x) => x.data)
 	};
 };
