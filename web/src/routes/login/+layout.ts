de18fad534224491ebc6d44d07aa5cd4055f3371
maxid <97409287+maxdorninger@users.noreply.github.com>
2025-12-22 18:38:19 +0100

    fix login with oauth button not displaying

--- web/src/routes/login/+layout.ts
+++ web/src/routes/login/+layout.ts
@@ -3,5 +3,5 @@ import client from '$lib/api';
 
 export const load: LayoutLoad = async ({ fetch }) => {
 	const { data } = await client.GET('/api/v1/auth/metadata', { fetch: fetch });
-	return { oauthProviders: data };
+	return { loginData: data };
 };
